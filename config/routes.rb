45bf5af269bdca9fad02448419f27ce6fbb61f8a
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-02-24 17:11:58 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :ratings
-  resources :words
   resources :users
+  
+  resources :words do
+    resources :ratings
+
+  resources :games
+  end
+  
+  #original setup
+    # resources :games
+    # resources :ratings
+    # resources :words
+    # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
