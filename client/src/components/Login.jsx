4f5b1f288dc5dda2a86a86d094ab1e8798545681
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-02-27 10:03:28 -0500

    start from sunday

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,23 +3,37 @@ import { loginUser } from '../services/users'
 import { useNavigate } from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 
 export default function Login(props) {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-
   const navigate = useNavigate()
+  //setup for toast error handling
+  const [show, setShow] = useState(true)
 
   return (
     <Form onSubmit={async (e) => {
-      e.preventDefault()
-      const user = {
-        username,
-        password
+      try {
+        e.preventDefault()
+        const user = {
+          username,
+          password
+        }
+        const resp = await loginUser(user)
+        props.setCurrentUser(resp)
+        navigate('/')
+      } catch (err) {
+        <>
+          alert('user cannot login')
+        {/* //set show state */}
+        <h1>What</h1>
+        <Alert show={show} variant="danger">
+          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        </Alert>
+          console.log(err)
+          </>
       }
-      const resp = await loginUser(user)
-      props.setCurrentUser(resp)
-      navigate('/')
     }}>
       <Form.Group>
         <Form.Label>username</Form.Label>
--- client/src/screens/Words.jsx
+++ client/src/screens/Words.jsx
@@ -4,9 +4,6 @@ export default function Words(props) {
   return (
     <div>
       <h2>Word List</h2>
-      {/* { props.currentUser &&
-        <Link to='/words/create'>Create A Word!!!</Link>
-      } */}
       {
       props.words.map(word => (
         <Link key={word.id} to={`/words/${word.id}`}>
