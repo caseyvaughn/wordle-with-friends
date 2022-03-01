2d365e359d06ed31ab7ece58761a8023df2a9442
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-02-28 20:29:24 -0500

    end of monday

--- client/src/components/Game/Keyboard.jsx
+++ client/src/components/Game/Keyboard.jsx
@@ -6,7 +6,6 @@ import { useEffect } from 'react'
 export default function Keyboard({ boardData, handleKeyboard }) {
     function handleKey(key) { 
       if (key.key === "Enter") {
-        console.log({ key})
         handleKeyboard("ENTER")
       }
         if (key.key === "Backspace")
@@ -18,7 +17,7 @@ export default function Keyboard({ boardData, handleKeyboard }) {
         window.addEventListener("keydown", handleKey)
         return () => { window.removeEventListener("keydown", handleKey) }
     }, [handleKeyboard])
-  console.log(keys)
+  
  
   
   return (
@@ -26,15 +25,16 @@ export default function Keyboard({ boardData, handleKeyboard }) {
       <div className="row">
         {keys.map((item, index) => {
           //maps over the 3 key arrays; 1 for each keyboard row!
-        console.log(item)
         return <div className="keyboard-rows" key={index} >
           {item.map((key, keyIndex) => { 
-              return (
-                <button key={keyIndex} 
+            return (
+              <button key={keyIndex}
                 // className={"key-correct"}
-                className={"key-button", `${boardData && boardData.correctCharArray.includes(key) ? "key-correct" :
+                className={`key-button
+                  ${boardData && boardData.correctCharArray.includes(key) ? "key-correct" :
                             (boardData && boardData.presentCharArray.includes(key) ? "key-present" :
-                            boardData && boardData.absentCharArray.includes(key) ? "key-absent" :"")} `} 
+                      boardData && boardData.absentCharArray.includes(key) ? "key-absent" : "")}`
+                } 
                     onClick={()=>{handleKeyboard(key)}}>
                     {key}
                 </button>
