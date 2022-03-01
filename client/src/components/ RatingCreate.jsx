17aa72de1dcfc0cbc56ef185c2e9c59618fbcbb2
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-03-01 17:27:37 -0500

    commit before changing status variable

--- client/src/components/ RatingCreate.jsx
+++ client/src/components/ RatingCreate.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import FormCheck from 'react-bootstrap/FormCheck'
 import RangeSlider from "react-bootstrap-range-slider"
 //github page with react bootstrap rangeslider examples:
 //https://jaywilz.github.io/react-bootstrap-range-slider/
 
 export default function RatingCreate(props) {
-  const [difficulty_rating, setDifficulty_rating] = useState(0)
+  const [difficulty_rating, setDifficulty_rating] = useState('')
   // const [is_real_word, setIs_real_word]=useState(null)
-  
+
     return (
       <div>
         <h2>RatingCreate.jsx</h2>
@@ -21,9 +20,8 @@ export default function RatingCreate(props) {
           }
           props.handleRatingCreate(rating)
         }}>
-         
           <Form.Group>
-            <Form.Label>Please rate the word's difficulty easiest to hardest</Form.Label>
+            <Form.Label>Please rate the word's difficulty easiest(1) to hardest (5)</Form.Label>
             <p></p>
               <RangeSlider
                 value={difficulty_rating}
@@ -32,13 +30,6 @@ export default function RatingCreate(props) {
                 max={5}
               />
             </Form.Group>
-          {/* <Form.Group>
-            <Form.Check
-              type="checkbox"
-              checked={false}
-              //on change to toggle this!!!!!
-              label="Flag word"/>
-          </Form.Group> */}
         </Form>
         <p></p>
         <Button size="sm" type="submit" style={{"width":"20%"}}>submit rating</Button>
