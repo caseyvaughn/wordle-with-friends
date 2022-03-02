a89f3b91f87269d5506b42ecacd55ac36dd105c1
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-03-02 10:29:36 -0500

    changed back to local host

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://wordle-with-friends-api.herokuapp.com/' : 'http://localhost:3000' 
+// const baseURL = process.env.NODE_ENV === 'production' ? 'https://wordle-with-friends-api.herokuapp.com/' : 'http://localhost:3000' 
+const baseURL = 'http://localhost:3000'
 
 export const api = axios.create({
   baseURL
 })
 
-// const baseURL = 'http://localhost:3000'
+
 
 // export const api = axios.create({
 //   baseURL
