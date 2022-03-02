709ecacc15e3146fbe30f6abded68b4693eff335
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-03-02 09:36:58 -0500

    added deployed link to api config

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,8 +1,14 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000'
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://wordle-with-friends-api.herokuapp.com/' : 'http://localhost:3000' 
 
 export const api = axios.create({
-  baseURL
-  // baseURL: baseURL
+  baseURL: baseURL
 })
+
+// const baseURL = 'http://localhost:3000'
+
+// export const api = axios.create({
+//   baseURL
+//   // baseURL: baseURL
+// })
