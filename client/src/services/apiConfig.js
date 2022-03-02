2c0f6052236b3ba8a5ba49185e46583f2e2415b7
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-03-02 10:19:22 -0500

    changed baseURL

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseURL = process.env.NODE_ENV === 'production' ? 'https://wordle-with-friends-api.herokuapp.com/' : 'http://localhost:3000' 
 
 export const api = axios.create({
-  baseURL: baseURL
+  baseURL
 })
 
 // const baseURL = 'http://localhost:3000'
